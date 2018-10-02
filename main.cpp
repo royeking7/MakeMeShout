989571bcc6732a5d700481ace248d16a88e2b159
royeking7 <rking27@mail.csuchico.edu>
2018-10-02 09:43:14 -0700

    Makemeshout

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Kevin Buffardi and class
+//Roy King
 #include <iostream>
 #include <string>
 
@@ -10,7 +10,17 @@ int main()
 
   cout<<"What do you say?\n";
   getline(cin,said);
-
+  
+  for(int index=0; index<said.length(); index++)
+  {
+     char converted = said[index];
+     if(said[index]>='a' && said[index]<='z')
+     {
+       converted=said[index] - ('a' - 'A');
+     }
+    said[index]=converted;
+    //cout<<said[index]<<" "<<said[index]-32<<endl;
+  }  
   cout<<"As a shout, you said:\n"<<said<<endl;
 
   return 0;
